b051c438662223a171808d8e33f1877d0a959e1e
zulfadli4 <160456995+zulfadli4@users.noreply.github.com>
2024-02-26 22:33:39 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 $host = "127.0.0.1";
 $username = "root";
 $password = "";
-$database_name = "noval";
+$database_name = "ukk_fadli";
 $connection = mysqli_connect($host, $username, $password, $database_name);
 
 /* SIGN UP Member */
